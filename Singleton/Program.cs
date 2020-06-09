0cc57ab4f73cf5dac6f68d4c6bed62a142c383d1
Alan Vieceli <alan.vieceli@softplan.com.br>
2020-06-09 19:18:25 -0300

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.GetInstancia.Mensagem("Teste 1");
+            Singleton.GetInstancia.Mensagem("Teste 2");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instacia = null;
+
+        public static Singleton GetInstancia
+        {
+            get
+            {
+                if (_instacia == null)
+                {
+                    Console.WriteLine("Criou Instancia");
+                    _instacia = new Singleton();
+                }
+                Console.WriteLine("Pegou Instancia");
+                return _instacia;
+            }
+
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        
+    }
+}
